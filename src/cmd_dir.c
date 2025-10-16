b101f69ff7fabba154e42b508c0422d2daa0e5b3
j777ferrol-debug <j777ferrol@gmail.com>
2025-10-16 13:01:50 +0200

    group and user name implemented in dir

--- src/cmd_dir.c
+++ src/cmd_dir.c
@@ -8,14 +8,17 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 #include <unistd.h>
 #include <time.h>
 #include <stdbool.h>
 #include <limits.h>
+#include <grp.h>
+#include <pwd.h>
 
 /* Forward declarations */
-static void ListarFichero(const char *path, tLengthFormat, tLinkDestination);
-static void ListarDirectorio(const char *dir, tLengthFormat, tLinkDestination, tListHidden, tRecursiveMode, int depth);
+void ListarFichero(const char *path, tLengthFormat, tLinkDestination);
+void ListarDirectorio(const char *dir, tLengthFormat, tLinkDestination, tListHidden, tRecursiveMode, int depth);
 
 void Cmd_dir(char *tr[], dirParams *params)
 {
@@ -37,10 +40,43 @@ void Cmd_dir(char *tr[], dirParams *params)
     }
 }
 
-static void ListarFichero(const char *path, tLengthFormat longfmt, tLinkDestination linkinfo)
+char LetraTF (mode_t m)
 {
-    (void)linkinfo;
+    switch (m&S_IFMT) { /*and bit a bit con los bits de formato,0170000 */
+        case S_IFSOCK: return 's'; /*socket */
+        case S_IFLNK: return 'l'; /*symbolic link*/
+        case S_IFREG: return '-'; /* fichero normal*/
+        case S_IFBLK: return 'b'; /*block device*/
+        case S_IFDIR: return 'd'; /*directorio */ 
+        case S_IFCHR: return 'c'; /*char device*/
+        case S_IFIFO: return 'p'; /*pipe*/
+        default: return '?'; /*desconocido, no deberia aparecer*/
+    }
+}
 
+char * ConvierteModo (mode_t m, char *permisos)
+{
+    strcpy (permisos,"---------- ");
+    
+    permisos[0]=LetraTF(m);
+    if (m&S_IRUSR) permisos[1]='r';    /*propietario*/
+    if (m&S_IWUSR) permisos[2]='w';
+    if (m&S_IXUSR) permisos[3]='x';
+    if (m&S_IRGRP) permisos[4]='r';    /*grupo*/
+    if (m&S_IWGRP) permisos[5]='w';
+    if (m&S_IXGRP) permisos[6]='x';
+    if (m&S_IROTH) permisos[7]='r';    /*resto*/
+    if (m&S_IWOTH) permisos[8]='w';
+    if (m&S_IXOTH) permisos[9]='x';
+    if (m&S_ISUID) permisos[3]='s';    /*setuid, setgid y stickybit*/
+    if (m&S_ISGID) permisos[6]='s';
+    //if (m&S_ISVTX) permisos[9]='t';
+    
+    return permisos;
+}
+
+void ListarFichero(const char *path, tLengthFormat longfmt, tLinkDestination linkinfo)
+{
     struct stat st;
 	if ((linkinfo ==LINK && stat(path, &st) == -1) || (linkinfo == NOLINK && lstat(path, &st) == -1)) {
         perror(path);
@@ -56,21 +92,15 @@ static void ListarFichero(const char *path, tLengthFormat longfmt, tLinkDestinat
     struct tm *tm = localtime(&st.st_mtime);
     strftime(timebuf, sizeof(timebuf), "%Y-%m-%d %H:%M", tm);
 	
-    printf("%s %3ld ",	timebuf, (long) st.st_nlink);
-
-    putchar(S_ISDIR(st.st_mode) ? 'd' : (S_ISLNK(st.st_mode) ? 'l' : '-'));
-    putchar(st.st_mode & S_IRUSR ? 'r' : '-');
-    putchar(st.st_mode & S_IWUSR ? 'w' : '-');
-    putchar(st.st_mode & S_IXUSR ? 'x' : '-');
-    putchar(st.st_mode & S_IRGRP ? 'r' : '-');
-    putchar(st.st_mode & S_IWGRP ? 'w' : '-');
-    putchar(st.st_mode & S_IXGRP ? 'x' : '-');
-    putchar(st.st_mode & S_IROTH ? 'r' : '-');
-    putchar(st.st_mode & S_IWOTH ? 'w' : '-');
-    putchar(st.st_mode & S_IXOTH ? 'x' : '-');
-    putchar(' ');
-
-    printf("%8ld %s\n",	(long) st.st_size, path);
+	char permisos[12];
+	ConvierteModo(st.st_mode, permisos);
+	
+	struct passwd pwd;
+	struct group grp;
+	pwd = *getpwuid(st.st_uid);
+	grp = *getgrgid(st.st_gid);
+	
+    printf("%s %d %3ld %s %s %s %8ld %s\n",	timebuf, (int) st.st_ino, (long) st.st_nlink, pwd.pw_name, grp.gr_name, permisos, (long) st.st_size, path);
 
     if (S_ISLNK(st.st_mode) && linkinfo) {
         char target[PATH_MAX];
@@ -82,7 +112,7 @@ static void ListarFichero(const char *path, tLengthFormat longfmt, tLinkDestinat
     }
 }
 
-static void ListarDirectorio(const char *dir, tLengthFormat longfmt, tLinkDestination linkinfo, tListHidden showhid, tRecursiveMode recmode, int depth)
+void ListarDirectorio(const char *dir, tLengthFormat longfmt, tLinkDestination linkinfo, tListHidden showhid, tRecursiveMode recmode, int depth)
 {
     DIR *dp;
     struct dirent *entry;
