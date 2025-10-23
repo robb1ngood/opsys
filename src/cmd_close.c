d48fa5e7c02f828a1e25dbf6a76e592810920b19
j777ferrol-debug <j777ferrol@gmail.com>
2025-10-23 10:48:41 +0200

    fixed close

--- src/cmd_close.c
+++ src/cmd_close.c
@@ -10,14 +10,14 @@ void Cmd_close (char *tr[], tFileList *fl)
 	
 	char* endptr;
 	int df = strtol(tr[1], &endptr, 10);
-	if (fl == NULL || fl->last == LNULL) {
+	if (tr[1] == endptr || df <= 2) {	/*parametro incorrecto*/
 		Cmd_listopen(tr, fl);
 		return;
 	}
 	
 	int pos = file_find_descriptor(*fl, df);
 	if (pos == LNULL) {
-		perror("Unable to close descriptor");
+		fprintf(stderr, "Unable to close descriptor: file is not open\n");
 		return;
 	}
 	
