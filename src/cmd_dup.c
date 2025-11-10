7352cab80aa329d267b8b6e592472767a87793ee
j777ferrol-debug <j777ferrol@gmail.com>
2025-11-10 10:45:21 +0100

    better listopen
    closer to the reference shell

--- src/cmd_dup.c
+++ src/cmd_dup.c
@@ -13,10 +13,6 @@ void Cmd_dup (int n, char * tr[], tFileList *fl)
 	
 	char *endptr, aux[PATH_MAX];
 	int df = strtol(tr[1], &endptr, 10), duplicado;
-	if (fl == NULL || fl->last == LNULL) { //empty list
-		Cmd_listopen(n, tr, fl);
-		return;
-	}
 	if (tr[1] == endptr || df < 0) {/*parametro invalido*/
 		Cmd_listopen(n, tr, fl);	/*o el descriptor es menor que 0*/
 		return;
@@ -26,8 +22,8 @@ void Cmd_dup (int n, char * tr[], tFileList *fl)
 		perror("imposible duplicar descriptor");
 	else {
 		int pos = file_find_descriptor(*fl, df);
-		if (pos == LNULL || pos < 0 || pos > fl->last) {
-			perror("imposible duplicar descriptor");
+		if (pos == LNULL) {
+			fprintf(stderr, "imposible duplicar descriptor");
 			return;
 		}
 		snprintf(aux, sizeof(aux), "dup %d (%s)", df, file_get(*fl, pos).name);
