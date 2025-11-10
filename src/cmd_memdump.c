678a9f4f6f7e7cf42d195d6c3ebc894634d43603
j777ferrol-debug <j777ferrol@gmail.com>
2025-11-10 09:19:45 +0100

    memdump

--- src/cmd_memdump.c
+++ src/cmd_memdump.c
@@ -1,3 +1,32 @@
 #include "commands.h"
 
-void Cmd_memdump(int n, char *tr[]) {}
+void Cmd_memdump(int n, char *tr[]) {
+	if (n != 3) {
+		fprintf(stderr, "usage: memdump <addr> <cont>\n");
+		return;
+	}
+	
+	char *endptr;
+	unsigned char *arr = (unsigned char *)strtol(tr[1], &endptr, 16);
+	if (*endptr != '\0') {
+		fprintf(stderr, "invalid adress %s", tr[1]);
+		return;
+	}
+	
+	size_t cont = (size_t)strtol(tr[2], &endptr, 10);
+	if (*endptr != '\0') {
+		fprintf(stderr, "invalid size %s", tr[2]);
+		return;
+	}
+	
+	for (size_t i = 0; i < cont; i++) {
+		unsigned char c = arr[i];
+		switch(c) {
+			case '\n': fputs("\\n", stdout); break;
+			case '\t': fputs("\\t", stdout); break;
+			case '\r': fputs("\\r", stdout); break;
+			default: fputc(c, stdout); break;
+		}
+	}
+	fputc('\n', stdout);
+}
